11360769f4f39874179349ca219c400e8b91f5d8
iamsahu <praffulsahu@gmail.com>
2022-06-09 00:55:46 +0530

    feat: completed the `unwinding`

--- contracts/YieldStEthLever.sol
+++ contracts/YieldStEthLever.sol
@@ -4,21 +4,81 @@ pragma solidity ^0.8.14;
 import "erc3156/contracts/interfaces/IERC3156FlashBorrower.sol";
 import "erc3156/contracts/interfaces/IERC3156FlashLender.sol";
 import "@yield-protocol/yieldspace-interfaces/IPool.sol";
-import "@yield-protocol/vault-interfaces/src/ILadle.sol";
+// import "@yield-protocol/vault-interfaces/src/ILadle.sol";
+import "@yield-protocol/vault-interfaces/src/ICauldron.sol";
+import "@yield-protocol/vault-interfaces/src/DataTypes.sol";
 import "@yield-protocol/utils-v2/contracts/token/IERC20.sol";
 import "@yield-protocol/vault-v2/other/lido/StEthConverter.sol";
 import "@yield-protocol/vault-v2/utils/Giver.sol";
+import "@yield-protocol/vault-v2/FlashJoin.sol";
 import "./interfaces/IStableSwap.sol";
-import "forge-std/Test.sol";
+
+interface YieldLadle {
+    function pools(bytes6 seriesId) external view returns (address);
+
+    function build(
+        bytes6 seriesId,
+        bytes6 ilkId,
+        uint8 salt
+    ) external payable returns (bytes12, DataTypes.Vault memory);
+
+    function repay(
+        bytes12 vaultId_,
+        address to,
+        int128 ink,
+        uint128 min
+    ) external payable returns (uint128 art);
+
+    function repayVault(
+        bytes12 vaultId_,
+        address to,
+        int128 ink,
+        uint128 max
+    ) external payable returns (uint128 base);
+
+    function close(
+        bytes12 vaultId_,
+        address to,
+        int128 ink,
+        int128 art
+    ) external payable returns (uint128 base);
+
+    function give(bytes12 vaultId_, address receiver)
+        external
+        payable
+        returns (DataTypes.Vault memory vault);
+
+    function pour(
+        bytes12 vaultId,
+        address to,
+        int128 ink,
+        int128 art
+    ) external payable;
+
+    function repayFromLadle(bytes12 vaultId_, address to)
+        external
+        payable
+        returns (uint256 repaid);
+
+    function closeFromLadle(bytes12 vaultId_, address to)
+        external
+        payable
+        returns (uint256 repaid);
+}
 
 contract YieldStEthLever is IERC3156FlashBorrower {
     IERC3156FlashLender fyToken;
-    ILadle constant ladle = ILadle(0x6cB18fF2A33e981D1e38A663Ca056c0a5265066A);
+    YieldLadle constant ladle =
+        YieldLadle(0x6cB18fF2A33e981D1e38A663Ca056c0a5265066A);
+    ICauldron constant cauldron =
+        ICauldron(0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867);
     IStableSwap stableSwap =
         IStableSwap(0x828b154032950C8ff7CF8085D841723Db2696056);
     StEthConverter constant stEthConverter =
         StEthConverter(0x93D232213cCA6e5e7105199ABD8590293C3eb106);
     bytes6 constant ilkId = bytes6(0x303400000000); //wsteth
+    FlashJoin constant flashJoin =
+        FlashJoin(0x5364d336c2d2391717bD366b29B6F351842D7F82);
     IPool pool = IPool(0xc3348D8449d13C364479B1F114bcf5B73DFc0dc6);
     Giver giver;
 
@@ -27,14 +87,29 @@ contract YieldStEthLever is IERC3156FlashBorrower {
         fyToken = fyToken_;
         IERC20(address(fyToken_)).approve(address(pool), type(uint256).max);
         pool.base().approve(address(stableSwap), type(uint256).max);
+        IERC20(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0).approve(
+            address(stableSwap),
+            type(uint256).max
+        ); //wsteth
+        IERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84).approve(
+            address(stableSwap),
+            type(uint256).max
+        ); //steth
+        IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2).approve(
+            0x3bDb887Dc46ec0E964Df89fFE2980db0121f0fD0, //join
+            type(uint256).max
+        ); //weth
+        IERC20(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0).approve(
+            0x5364d336c2d2391717bD366b29B6F351842D7F82, //flashjoin
+            type(uint256).max
+        ); //wsteth
     }
 
     function invest(
         uint256 baseAmount,
         uint128 borrowAmount,
-        uint128 maxFyAmount,
         bytes6 seriesId
-    ) external {
+    ) external returns (bytes12) {
         IERC20(address(fyToken)).transferFrom(
             msg.sender,
             address(this),
@@ -46,10 +121,11 @@ contract YieldStEthLever is IERC3156FlashBorrower {
             this, // Loan Receiver
             address(fyToken), // Loan Token
             investAmount, // Loan Amount
-            abi.encode(investAmount, borrowAmount, maxFyAmount, vaultId)
+            abi.encode(0, abi.encode(borrowAmount, vaultId))
         );
         require(success, "Failed to flash loan");
         giver.give(vaultId, msg.sender);
+        return vaultId;
     }
 
     function onFlashLoan(
@@ -57,29 +133,54 @@ contract YieldStEthLever is IERC3156FlashBorrower {
         address token,
         uint256 amount, // Amount of FYToken received
         uint256 fee,
-        bytes calldata data
+        bytes memory data
     ) external returns (bytes32) {
         address thisAdd = address(this);
-        require(
-            msg.sender == address(fyToken),
-            "FlashBorrower: Untrusted lender"
-        );
+        // require(
+        //     msg.sender == address(fyToken),
+        //     "FlashBorrower: Untrusted lender"
+        // );
         require(
             initiator == thisAdd,
             "FlashBorrower: Untrusted loan initiator"
         );
 
-        (, uint128 borrowAmount, , bytes12 vaultId) = abi.decode(
+        (uint128 status, bytes memory data2) = abi.decode(
             data,
-            (uint256, uint128, uint128, bytes12)
+            (uint128, bytes)
         );
-        IERC20(address(fyToken)).transfer(address(pool), amount);
+        if (status == 0) {
+            leverUp(
+                amount, // Amount of FYToken received
+                fee,
+                data2
+            );
+        } else if (status == 1) {
+            doRepay(amount, fee, data2);
+        } else if (status == 2) {
+            doClose(amount, fee, data2);
+        }
+        return keccak256("ERC3156FlashBorrower.onFlashLoan");
+    }
+
+    function leverUp(
+        uint256 amount, // Amount of FYToken received
+        uint256 fee,
+        bytes memory data
+    ) internal {
+        address thisAdd = address(this);
+
+        (uint128 borrowAmount, bytes12 vaultId) = abi.decode(
+            data,
+            (uint128, bytes12)
+        );
+        IERC20(address(fyToken)).transfer(address(pool), amount - fee);
 
         // Get WETH
         uint128 baseReceived = pool.buyBase(
             thisAdd,
-            uint128(pool.sellFYTokenPreview(uint128(amount))),
-            uint128(amount)
+            uint128(pool.sellFYTokenPreview(uint128(amount - fee))),
+            uint128(amount - fee)
         );
         // Swap WETH for stETH on curve
         // 0: WETH
@@ -90,7 +191,7 @@ contract YieldStEthLever is IERC3156FlashBorrower {
             pool.base().balanceOf(thisAdd), // This value is different from base received
             1
         );
-        console.log(IERC20(address(fyToken)).balanceOf(thisAdd));
+
         // Wrap steth to wsteth
         IERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84).transfer(
             address(stEthConverter),
@@ -99,69 +200,158 @@ contract YieldStEthLever is IERC3156FlashBorrower {
         uint256 wrappedamount = stEthConverter.wrap(
             0x5364d336c2d2391717bD366b29B6F351842D7F82
         );
-        // Deposit wstETH in the vault & borrow fyToken
+
+        // Deposit wstETH in the vault & borrow fyToken to payback
         ladle.pour(
             vaultId,
             thisAdd,
             int128(uint128(wrappedamount)),
-            int128(uint128(borrowAmount)) // How much could I borrow?
+            int128(uint128(borrowAmount))
         );
-        return keccak256("ERC3156FlashBorrower.onFlashLoan");
     }
 
-    // function unwind(
-    //     bytes12 vaultId,
-    //     uint256 maxAmount,
-    //     uint128 ink,
-    //     uint128 art,
-    //     bytes6 seriesId
-    // ) external {
-    //     Vault memory vault_ = cauldron.vaults(vaultId);
-    //     Series memory series_ = cauldron.series(seriesId);
+    function unwind(
+        bytes12 vaultId,
+        uint256 maxAmount,
+        uint128 ink,
+        uint128 art,
+        bytes6 seriesId
+    ) external {
+        DataTypes.Vault memory vault_ = cauldron.vaults(vaultId);
+        DataTypes.Series memory series_ = cauldron.series(seriesId);
 
-    //     // Test that the caller is the owner of the vault.
-    //     // This is important as we will take the vault from the user.
-    //     require(vault_.owner == msg.sender);
+        // Test that the caller is the owner of the vault.
+        // This is important as we will take the vault from the user.
+        require(vault_.owner == msg.sender);
 
-    //     // Give the vault to the contract
-    //     giver.give(vaultId, address(this));
+        // Give the vault to the contract
+        giver.seize(vaultId, address(this));
+        bool success;
+        if (uint32(block.timestamp) < series_.maturity) {
+            // Series is not past maturity
+            // Borrow to repay debt, move directly to the pool.
+            success = fyToken.flashLoan(
+                this, // Loan Receiver
+                address(fyToken), // Loan Token
+                maxAmount, // Loan Amount
+                abi.encode(
+                    1,
+                    abi.encode(msg.sender, vaultId, maxAmount, ink, art)
+                )
+            );
+        } else {
+            // Series is past maturity, borrow and move directly to collateral pool
+            uint128 base = cauldron.debtToBase(seriesId, art);
+            bytes memory data = abi.encode(
+                msg.sender,
+                vaultId,
+                maxAmount,
+                base,
+                ink,
+                art
+            );
+            // wsteth flash join 0x5364d336c2d2391717bD366b29B6F351842D7F82
+            success = flashJoin.flashLoan(
+                this, // Loan Receiver
+                0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0, // Loan Token
+                base, // Loan Amount
+                abi.encode(2, data)
+            );
+        }
+        require(success, "Failed to flash loan");
 
-    //     if (uint32(block.timestamp) < series_.maturity) {
-    //         // Series is not past maturity
-    //         // Borrow to repay debt, move directly to the pool.
-    //         iUSDC.flashBorrow(
-    //             maxAmount,
-    //             pool,
-    //             address(this),
-    //             "",
-    //             abi.encodeWithSignature(
-    //                 "doRepay(address,bytes12,uint256,uint128)",
-    //                 msg.sender,
-    //                 vaultId,
-    //                 maxAmount,
-    //                 ink
-    //             )
-    //         );
-    //     } else {
-    //         // Series is past maturity, borrow and move directly to collateral pool
-    //         uint128 base = cauldron.debtToBase(seriesId, art);
-    //         iUSDC.flashBorrow(
-    //             base,
-    //             usdcJoin,
-    //             address(this),
-    //             "",
-    //             abi.encodeWithSignature(
-    //                 "doClose(address,bytes12,uint128,uint128,uint128)",
-    //                 msg.sender,
-    //                 vaultId,
-    //                 base,
-    //                 ink,
-    //                 art
-    //             )
-    //         );
-    //     }
+        // Give the vault back to the sender, just in case there is anything left
+        giver.give(vaultId, msg.sender);
+        // Transferring the leftover to the borrower
+        IERC20(address(fyToken)).transfer(
+            msg.sender,
+            IERC20(address(fyToken)).balanceOf(address(this))
+        );
+    }
 
-    //     // Give the vault back to the sender, just in case there is anything left
-    //     giver.give(vaultId, msg.sender);
-    // }
+    function doRepay(
+        uint256 amount, // Amount of FYToken received
+        uint256 fee,
+        bytes memory data
+    ) internal {
+        (address borrower, bytes12 vaultId, , uint128 ink, uint128 art) = abi
+            .decode(data, (address, bytes12, uint256, uint128, uint128));
+
+        IERC20(address(fyToken)).approve(address(ladle), art);
+        ladle.pour(
+            vaultId,
+            address(this),
+            -int128(ink),
+            -int128(uint128(art)) // How much could I borrow?
+        );
+
+        // Convert wsteth - steth
+        IERC20(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0).transfer(
+            address(stEthConverter),
+            ink
+        );
+        stEthConverter.unwrap(address(this));
+        // convert steth- weth
+        // 0: WETH
+        // 1: STETH
+        stableSwap.exchange(
+            1,
+            0,
+            IERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84).balanceOf(
+                address(this)
+            ), // balance of steth
+            1
+        );
+        uint128 wethToTran = pool.buyFYTokenPreview(uint128(amount + fee));
+        IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2).transfer(
+            address(pool),
+            wethToTran
+        );
+        pool.sellBase(address(this), wethToTran);
+        // Transferring the leftover to the borrower
+        IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2).transfer(
+            borrower,
+            IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2).balanceOf(
+                address(this)
+            )
+        );
+    }
+
+    function doClose(
+        uint256 amount, // Amount of FYToken received
+        uint256 fee,
+        bytes memory data
+    ) internal {
+        (
+            address owner,
+            bytes12 vaultId,
+            uint256 maxAmount,
+            uint128 base,
+            uint128 ink,
+            uint128 art
+        ) = abi.decode(
+                data,
+                (address, bytes12, uint256, uint128, uint128, uint128)
+            );
+
+        // Convert wsteth - steth
+        IERC20(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0).transfer(
+            address(stEthConverter),
+            maxAmount
+        );
+        stEthConverter.unwrap(address(this));
+        // convert steth- weth
+        // 0: WETH
+        // 1: STETH
+        stableSwap.exchange(
+            1,
+            0,
+            IERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84).balanceOf(
+                address(this)
+            ), // balance of steth
+            1
+        );
+
+        ladle.close(vaultId, address(this), -int128(ink), -int128(art));
+    }
 }
--- test/YieldStEthLever.t.sol
+++ test/YieldStEthLever.t.sol
@@ -9,7 +9,10 @@ import "@yield-protocol/utils-v2/contracts/token/IERC20.sol";
 import "@yield-protocol/utils-v2/contracts/access/AccessControl.sol";
 import "./Protocol.sol";
 import "@yield-protocol/vault-v2/utils/Giver.sol";
+import "@yield-protocol/vault-v2/FlashJoin.sol";
 import "@yield-protocol/vault-interfaces/src/ICauldron.sol";
+import "@yield-protocol/yieldspace-interfaces/IPool.sol";
+
 // fyeth 0x53358d088d835399F1E97D2a01d79fC925c7D999
 contract YieldStEthLeverTest is Test {
     address timeLock = 0x3b870db67a45611CF4723d44487EAF398fAc51E3;
@@ -18,44 +21,117 @@ contract YieldStEthLeverTest is Test {
     address fyTokenWhale = 0x1c15b746360BB8E792C6ED8cB83f272Ce1D170E0;
     Protocol protocol;
     Giver giver;
+    bytes12 vaultId;
+    IPool pool = IPool(0xc3348D8449d13C364479B1F114bcf5B73DFc0dc6);
+    FlashJoin flashJoin;
 
     function setUp() public {
         protocol = new Protocol();
-        giver = new Giver(ICauldron(0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867));
+        giver = new Giver(
+            ICauldron(0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867)
+        );
         lever = new YieldStEthLever(
             IERC3156FlashLender(0x53358d088d835399F1E97D2a01d79fC925c7D999),
             giver
         );
         fyToken = FYToken(0x53358d088d835399F1E97D2a01d79fC925c7D999);
+        flashJoin = FlashJoin(0x5364d336c2d2391717bD366b29B6F351842D7F82);
         // Set the flash fee factor
         vm.prank(timeLock);
-        fyToken.setFlashFeeFactor(0);
+        fyToken.setFlashFeeFactor(1);
+
+        vm.prank(timeLock);
+        flashJoin.setFlashFeeFactor(1);
 
         //Label
         vm.label(address(lever), "YieldLever");
         vm.label(address(fyToken), "FYToken");
+        vm.label(0x3bDb887Dc46ec0E964Df89fFE2980db0121f0fD0, "WETH JOIN");
 
         vm.prank(fyTokenWhale);
-        fyToken.transfer(address(this), 1e18);
+        fyToken.transfer(address(this), 2e18);
+        // vm.prank(fyTokenWhale);
+        // fyToken.transfer(address(lever), 3e18);
 
         // Orchestrate Giver
-        AccessControl cauldronAccessControl = AccessControl(0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867);
+        AccessControl cauldronAccessControl = AccessControl(
+            0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867
+        );
         vm.prank(timeLock);
-        cauldronAccessControl.grantRole(0x798a828b,address(giver));
+        cauldronAccessControl.grantRole(0x798a828b, address(giver));
 
         AccessControl giverAccessControl = AccessControl(address(giver));
-        giverAccessControl.grantRole(0xe4fd9dc5,timeLock);
-
-
+        giverAccessControl.grantRole(0xe4fd9dc5, timeLock);
+        giverAccessControl.grantRole(0x35775afb, address(lever));
     }
 
-    function testLoan() public {
-        uint256 baseAmount = 0;
-        uint128 borrowAmount = 2e18;
+    function testLoanAndRepay() public {
+        uint256 baseAmount = 2e18;
+        uint128 borrowAmount = 4e18;
         uint128 maxFyAmount = 1e18;
         bytes6 seriesId = 0x303030370000;
         fyToken.approve(address(lever), baseAmount);
 
-        lever.invest(baseAmount, borrowAmount, maxFyAmount, seriesId);
+        vaultId = lever.invest(baseAmount, borrowAmount, seriesId);
+
+        DataTypes.Balances memory balances = ICauldron(
+            0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867
+        ).balances(vaultId);
+
+        lever.unwind(
+            vaultId,
+            balances.art,
+            balances.ink,
+            balances.art,
+            seriesId
+        );
+
+        balances = ICauldron(0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867)
+            .balances(vaultId);
+        assertEq(balances.art, 0);
+        assertEq(balances.ink, 0);
+    }
+
+    function testLoanAndClose() public {
+        uint256 baseAmount = 1e18;
+        uint128 borrowAmount = 3e18;
+        uint128 maxFyAmount = 1e18;
+        bytes6 seriesId = 0x303030370000;
+        fyToken.approve(address(lever), baseAmount);
+
+        vaultId = lever.invest(baseAmount, borrowAmount, seriesId);
+
+        DataTypes.Balances memory balances = ICauldron(
+            0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867
+        ).balances(vaultId);
+
+        // Travel to maturity
+        DataTypes.Series memory series_ = ICauldron(
+            0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867
+        ).series(seriesId);
+
+        vm.warp(series_.maturity);
+
+        DataTypes.Vault memory vault_ = ICauldron(
+            0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867
+        ).vaults(vaultId);
+        DataTypes.Debt memory debt_ = ICauldron(
+            0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867
+        ).debt(series_.baseId, vault_.ilkId);
+
+        // vm.prank(0x10CD5fbe1b404B7E19Ef964B63939907bdaf42E2);
+        // IERC20(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0).transfer(address(lever),10e18);
+        lever.unwind(
+            vaultId,
+            balances.art,
+            balances.ink,
+            balances.art,
+            seriesId
+        );
+
+        balances = ICauldron(0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867)
+            .balances(vaultId);
+        assertEq(balances.art, 0);
+        assertEq(balances.ink, 0);
     }
 }
