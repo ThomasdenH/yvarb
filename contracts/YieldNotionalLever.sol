d071604de54c89521a2f82a7b9403c0b4c1783ec
Thomas <denhollander.thomas@gmail.com>
2022-07-13 10:20:19 +0200

    Gas improvement

--- contracts/YieldNotionalLever.sol
+++ contracts/YieldNotionalLever.sol
@@ -9,8 +9,8 @@ import "@yield-protocol/vault-v2/other/notional/ERC1155.sol";
 contract YieldNotionalLever is YieldLeverBase, ERC1155TokenReceiver {
     using TransferHelper for IERC20;
     using TransferHelper for IFYToken;
-    FlashJoin usdcJoin;
-    FlashJoin daiJoin;
+    FlashJoin immutable usdcJoin;
+    FlashJoin immutable daiJoin;
 
     address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
